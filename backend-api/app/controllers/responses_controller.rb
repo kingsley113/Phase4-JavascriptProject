8a19ecb64af9870ebd22c07b875f95a1020df020
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-19 21:11:05 -0700

    Add response create action

--- backend-api/app/controllers/responses_controller.rb
+++ backend-api/app/controllers/responses_controller.rb
@@ -6,5 +6,10 @@ class ResponsesController < ApplicationController
 
 	def create
 		# Add code to create response and assign to user, saving question number 
+		response = Response.new
+		response.character_id = params[:character_id]
+		response.question_id = params[:question_id]
+		response.response = params[:response]
+		response.save
 	end
 end
