be0a8776eba84d70ba57484f62b7dd7ed779b0e8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-19 23:38:13 -0700

    Add phrases to questions migration

--- backend-api/db/migrate/20210920003525_create_questions.rb
+++ backend-api/db/migrate/20210920003525_create_questions.rb
@@ -9,6 +9,13 @@ class CreateQuestions < ActiveRecord::Migration[6.1]
       t.text :answer5
       t.text :answer6
 
+			t.text :phrase1
+			t.text :phrase2
+			t.text :phrase3
+			t.text :phrase4
+			t.text :phrase5
+			t.text :phrase6
+			
       t.timestamps
     end
   end
