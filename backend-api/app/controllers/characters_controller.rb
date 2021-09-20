7f366cf54c6b4a1617117858af5c0d9836315fc4
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-19 23:37:01 -0700

    Clean up comments

--- backend-api/app/controllers/characters_controller.rb
+++ backend-api/app/controllers/characters_controller.rb
@@ -4,7 +4,6 @@ class CharactersController < ApplicationController
 		# code for return list of all characters
 		characters = Character.all
 		render json: CharacterSerializer.new(characters).to_serialized_json
-		# add filters to this one to just show character name - break off to service class?
 	end
 	
 	def create 
@@ -18,7 +17,6 @@ class CharactersController < ApplicationController
 		# add code to show a character and thier responses
 		character = Character.find(params[:id])
 		render json: CharacterSerializer.new(character).to_serialized_json
-		# break this off into service class
 	end
 
 end
