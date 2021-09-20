f85e6d3c47139a14afbe5cc7d2afe641f756945d
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-20 14:19:07 -0700

    Develop Seed file

--- backend-api/db/seeds.rb
+++ backend-api/db/seeds.rb
@@ -5,3 +5,100 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Questions
+question1 = Question.new 
+question1.question = ""
+question1.answer1 = ""
+question1.phrase1 = ""
+question1.answer2 = ""
+question1.phrase2 = ""
+question1.answer3 = ""
+question1.phrase3 = ""
+question1.answer4 = ""
+question1.phrase4 = ""
+question1.answer5 = ""
+question1.phrase5 = ""
+question1.answer6 = ""
+question1.phrase6 = ""
+question1.save
+
+question2 = Question.new 
+question2.question = ""
+question2.answer1 = ""
+question2.phrase1 = ""
+question2.answer2 = ""
+question2.phrase2 = ""
+question2.answer3 = ""
+question2.phrase3 = ""
+question2.answer4 = ""
+question2.phrase4 = ""
+question2.answer5 = ""
+question2.phrase5 = ""
+question2.answer6 = ""
+question2.phrase6 = ""
+question2.save
+
+question3 = Question.new 
+question3.question = ""
+question3.answer1 = ""
+question3.phrase1 = ""
+question3.answer2 = ""
+question3.phrase2 = ""
+question3.answer3 = ""
+question3.phrase3 = ""
+question3.answer4 = ""
+question3.phrase4 = ""
+question3.answer5 = ""
+question3.phrase5 = ""
+question3.answer6 = ""
+question3.phrase6 = ""
+question3.save
+
+question4 = Question.new 
+question4.question = ""
+question4.answer1 = ""
+question4.phrase1 = ""
+question4.answer2 = ""
+question4.phrase2 = ""
+question4.answer3 = ""
+question4.phrase3 = ""
+question4.answer4 = ""
+question4.phrase4 = ""
+question4.answer5 = ""
+question4.phrase5 = ""
+question4.answer6 = ""
+question4.phrase6 = ""
+question4.save
+
+question5 = Question.new 
+question5.question = ""
+question5.answer1 = ""
+question5.phrase1 = ""
+question5.answer2 = ""
+question5.phrase2 = ""
+question5.answer3 = ""
+question5.phrase3 = ""
+question5.answer4 = ""
+question5.phrase4 = ""
+question5.answer5 = ""
+question5.phrase5 = ""
+question5.answer6 = ""
+question5.phrase6 = ""
+question5.save
+
+question6 = Question.new 
+question6.question = ""
+question6.answer1 = ""
+question6.phrase1 = ""
+question6.answer2 = ""
+question6.phrase2 = ""
+question6.answer3 = ""
+question6.phrase3 = ""
+question6.answer4 = ""
+question6.phrase4 = ""
+question6.answer5 = ""
+question6.phrase5 = ""
+question6.answer6 = ""
+question6.phrase6 = ""
+question6.save
