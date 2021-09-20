4d1207b8b399852ad1deea30f9d1abd083a4865a
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-20 16:18:24 -0700

    clean up comments

--- backend-api/app/controllers/characters_controller.rb
+++ backend-api/app/controllers/characters_controller.rb
@@ -1,20 +1,17 @@
 class CharactersController < ApplicationController
 
 	def index
-		# code for return list of all characters
 		characters = Character.all
 		render json: CharacterSerializer.new(characters).to_serialized_json
 	end
 	
 	def create 
-		# add code to create new character instance from name
 		character = Character.new
 		character.name = params[:name]
 		character.save
 	end
 
 	def show
-		# add code to show a character and thier responses
 		character = Character.find(params[:id])
 		render json: CharacterSerializer.new(character).to_serialized_json
 	end
