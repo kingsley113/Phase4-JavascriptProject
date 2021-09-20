312ea32f3f59ec1e38c0968e126412c3a52452a1
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-19 21:45:18 -0700

    Build out character serializer

--- backend-api/app/controllers/characters_controller.rb
+++ backend-api/app/controllers/characters_controller.rb
@@ -3,7 +3,7 @@ class CharactersController < ApplicationController
 	def index
 		# code for return list of all characters
 		characters = Character.all
-		render json: characters
+		render json: CharacterSerializer.new(characters).to_serialized_json
 		# add filters to this one to just show character name - break off to service class?
 	end
 	
@@ -17,7 +17,7 @@ class CharactersController < ApplicationController
 	def show
 		# add code to show a character and thier responses
 		character = Character.find(params[:id])
-		render json: characters
+		render json: CharacterSerializer.new(character).to_serialized_json
 		# break this off into service class
 	end
 
--- backend-api/app/services/characters_serializer.rb
+++ backend-api/app/services/characters_serializer.rb
@@ -3,7 +3,17 @@ class CharactersSerializer
 		@character = character_object
 	end
 
-	def serialize_characters
+	def to_serialized_json
+		@character.to_json(
+		# add json filters here
+		include: {
+			name:,
+			responses: {
+				only: [:response, :question_id]
+			},
+			except: [:updated_at, :created_at]
+		}
+		)
 	end
 	
 end
