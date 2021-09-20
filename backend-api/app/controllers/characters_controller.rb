166b44395586229b1175fcb462b01d30b4190dc6
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-19 21:34:54 -0700

    build out character controller & serializer

--- backend-api/app/controllers/characters_controller.rb
+++ backend-api/app/controllers/characters_controller.rb
@@ -2,14 +2,23 @@ class CharactersController < ApplicationController
 
 	def index
 		# code for return list of all characters
+		characters = Character.all
+		render json: characters
+		# add filters to this one to just show character name - break off to service class?
 	end
 	
 	def create 
 		# add code to create new character instance from name
+		character = Character.new
+		character.name = params[:name]
+		character.save
 	end
 
 	def show
 		# add code to show a character and thier responses
+		character = Character.find(params[:id])
+		render json: characters
+		# break this off into service class
 	end
 
 end
---
+++ backend-api/app/services/characters_serializer.rb
@@ -0,0 +1,9 @@
+class CharactersSerializer
+	def initialize(character_object)
+		@character = character_object
+	end
+
+	def serialize_characters
+	end
+	
+end
