81793fd8e0b783a055664d63211c7398b5fd86ac
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-20 19:38:43 -0700

    Revise Notes

--- backend-api/app/controllers/questions_controller.rb
+++ backend-api/app/controllers/questions_controller.rb
@@ -2,9 +2,17 @@ class QuestionsController < ApplicationController
 
 	def show
 		# find and return question based on id number provided
+		# binding.pry
 		question = Question.find(params[:id])
 		render json: question
 			# add options here to filter out unnessecary attributes
 			# remove created and updated times
 	end
+
+	# private
+
+	# def question_params
+	# 	params.require(:question).permit(:id)
+	# end
+
 end
