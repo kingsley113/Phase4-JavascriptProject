905b4e5692fd4efd01b2c8c3eafe698747f16d30
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-10-01 19:27:07 -0700

    Reconfigure for Postgresql Database

--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_20_025447) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.string "trait1"
